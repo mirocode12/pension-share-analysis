0afd844622bbd82de2e665bd344a65ed7c2f71d7
mirocode12 <miro.mandic.12@hotmail.com>
2023-04-09 03:04:44 +0100

    test

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+      <h1>dsfsdf</h1>
     </div>
   );
 }
